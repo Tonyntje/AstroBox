e91f99bb64d23718e17c5c23bab5da6b05569884
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2023-09-23 20:40:13 +0200

    Added posts and installed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
-import {netlifyFunctions} from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: netlifyFunctions()
+  adapter: node({
+    mode: "standalone"
+  })
 });
